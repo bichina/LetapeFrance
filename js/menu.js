94b2b3ad86105cd70c37c4a0b6fb0e9a69274df6
bichina <bichina0312@gmail.com>
2021-08-05 17:27:02 +0900

    teacher page modify

---
+++ js/menu.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".mobileMenu").addEventListener("click", function (e) {
+    if (document.querySelector(".menuwrap").classList.contains("on")) {
+      //메뉴닫힘
+      document.querySelector(".menuwrap").classList.remove("on");
+      document.querySelector(".mobileMenu img").classList.remove("fa-times");
+      document.querySelector(".mobileMenu img").classList.add("panelIcon");
+
+      //페이지 스크롤 락 해제
+      document.querySelector("#dimmed").remove();
+    } else {
+      //메뉴펼침
+      document.querySelector(".menuwrap").classList.add("on");
+      document.querySelector(".mobileMenu img").classList.remove("panelIcon");
+      document.querySelector(".mobileMenu img").classList.add("fa-times");
+
+      //페이지 스크롤 락 레이어 추가
+      let div = document.createElement("div");
+      div.id = "dimmed";
+      document.body.append(div);
+
+      //페이지 스크롤 락  모바일 이벤트 차단
+      document
+        .querySelector("#dimmed")
+        .addEventListener("scroll touchmove touchend mousewheel", function (e) {
+          e.preventDefault();
+          e.stopPropagation();
+          return false;
+        });
+
+      //페이지 스크롤 락 레이어 클릭 메뉴 닫기
+      document.querySelector("#dimmed").addEventListener("click", function (e) {
+        document.querySelector("..mobileMenu").click();
+      });
+    }
+  });
+});
