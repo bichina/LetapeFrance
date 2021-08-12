570c4cfdf98d74a216590d53e8020b285044e205
bichina <bichina0312@gmail.com>
2021-08-12 17:37:04 +0900

    메인화면 작업

--- js/menu.js
+++ js/menu.js
@@ -1,20 +1,27 @@
-//메뉴보기
-$(document).ready(function(){
- 
-    $('.mobileMenu').on('click', function(){
-        // $('.menu_bg').show(); 
-        $('.menuwrap').show().animate({
-            right:0
-        });  
-    });
-    $('.mobileMenuClose').on('click', function(){
-        // $('.menu_bg').hide(); 
-        $('.menuwrap').animate({
-            right: '-' + 100 + '%'
-                   },function(){
-$('.menuwrap').hide(); 
-}); 
-    });
+//이전 단계로 돌아가기
+function goBack() {
+  window.history.back();
+}
 
+//메뉴보기
+$(document).ready(function () {
+  $(".mobileMenu").on("click", function () {
+    // $('.menu_bg').show();
+    $(".menuwrap").show().animate({
+      right: 0,
+    });
+  });
+  $(".mobileMenuClose").on("click", function () {
+    // $('.menu_bg').hide();
+    $(".menuwrap").animate(
+      {
+        right: "-" + 100 + "%",
+      },
+      function () {
+        $(".menuwrap").hide();
+      }
+    );
+  });
 });
 
+//헤더 스크롤시 변하는 거
